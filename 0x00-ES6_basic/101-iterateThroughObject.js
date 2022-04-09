ad98511ee17587ab1c8be20c96bc0159c8024b7c
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-04-09 06:40:01 +0300

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,14 +1,5 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let i = 0;
-  let output = '';
-  for (const value in reportWithIterator) {
-    if (i !== reportWithIterator.length - 1) {
-      output += `${reportWithIterator[value]} | `;
-    } else {
-      output += reportWithIterator[value];
-    }
-    i += 1;
+  if (!reportWithIterator) {
+    console.log('Placeholder');
   }
-
-  return output;
 }
