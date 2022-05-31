5b84002c02c5438aee0ec277637fb29f80d6a446
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-05-31 23:32:43 +0300

    Hot fix

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,19 +1,42 @@
-const app = require('express')();
+const express = require('express');
 const countStudents = require('./3-read_file_async');
-const pathToCSVFile = process.argv[2];
 
-// Setup a listener
-app.listen(1245);
+const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello Holberton School!');
+  res.status(200);
+  res.set('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  countStudents(pathToCSVFile, 0).then((success) => {
-    const out = `This is the list of our students\n${success}`;
-    res.status(200).send(out);
-  });
+app.get('/students', async (req, res) => {
+  res.status(200);
+  res.set('Content-Type', 'text/plain');
+  await countStudents(process.argv[2])
+    .then((value) => {
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${value.lines.length}\n`);
+      res.write(
+        `Number of students in CS: ${value.cs.length}. List: ${value.cs.join(
+          ', '
+        )}\n`
+      );
+      res.write(
+        `Number of students in SWE: ${value.swe.length}. List: ${value.swe.join(
+          ', '
+        )}`
+      );
+      res.end();
+    })
+    .catch((err) => {
+      res.write('This is the list of our students\n');
+      res.end(err.message);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
 module.exports = app;
