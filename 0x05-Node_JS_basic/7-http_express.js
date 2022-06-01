e36ddcfef0ffa87cf552a48efd7ef907963764d8
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-06-01 05:43:47 +0300

    Hot fix

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,37 +1,60 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
+const countStudents = async (path) => {
+  try {
+    const csvData = await fs.promises.readFile(path, { encoding: 'utf8' });
+    const fields = {};
+    const dataShow = {};
+    let data = csvData.toString().split('\n');
+    data = data.filter((element) => element.length > 0);
+
+    data.shift();
+    data.forEach((element) => {
+      if (element.length > 0) {
+        const row = element.split(',');
+        if (row[3] in fields) {
+          fields[row[3]].push(row[0]);
+        } else {
+          fields[row[3]] = [row[0]];
+        }
+      }
+    });
+    dataShow.numberStudents = `Number of students: ${data.length}`;
+    dataShow.studentsFields = [];
+    for (const field in fields) {
+      if (field) {
+        const list = fields[field];
+        dataShow.studentsFields.push(
+          `Number of students in ${field}: ${list.length}. List: ${list
+            .toString()
+            .replace(/,/g, ', ')}`
+        );
+      }
+    }
+    return dataShow;
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
 const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-  res.status(200);
-  res.set('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  res.status(200);
-  res.set('Content-Type', 'text/plain');
-  await countStudents(process.argv[2])
-    .then((value) => {
-      res.write('This is the list of our students\n');
-      res.write(`Number of students: ${value.lines.length}\n`);
+app.get('/students', (req, res) => {
+  res.write('This is the list of our students\n');
+  countStudents(process.argv[2])
+    .then((dataShow) => {
       res.write(
-        `Number of students in CS: ${value.cs.length}. List: ${value.cs.join(
-          ', '
-        )}\n`
+        [dataShow.numberStudents].concat(dataShow.studentsFields).join('\n')
       );
-      res.write(
-        `Number of students in SWE: ${value.swe.length}. List: ${value.swe.join(
-          ', '
-        )}`
-      );
-      res.end();
+      res.end('\n');
     })
-    .catch((err) => {
-      res.write('This is the list of our students\n');
-      res.end(err.message);
+    .catch((error) => {
+      res.end(error.message);
     });
 });
 
